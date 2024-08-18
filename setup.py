ed1625b6d62b6415a106a995dc7e0e2063f097fd
Kasun Dulara <kasundularaam@gmail.com>
2024-08-19 02:52:51 +0530

    version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="ipqr",
-    version="0.1.0",
+    version="0.1.1",
     author="Kasun Dulara",
     author_email="kasundularaam@gmail.com",
     description="A CLI tool to generate QR codes for local server URLs",
